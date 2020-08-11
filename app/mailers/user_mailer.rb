701596e0196ae0abfc56c49a7b757b110361feaf
vergeade <fevergeade@gmail.com>
2020-08-11 11:24:14 +0200

    correction mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,16 @@
 class UserMailer < ApplicationMailer
-  default from: 'fev.no-reply@yopmail.com'
+  default from: 'fevergeade@gmail.com'
 
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Bienvenue par minou comme disent les chats')
+    mail(to: @user.email, subject: "Bienvenue #{@user.last_name} #{@user.first_name}")
   end
 
   def participation_email(attendance)
     @admin = User.find(attendance.event.admin_id)
     @event = Event.find(attendance.event.id)
     @participant = User.find(attendance.user.id)
-    mail(to: @admin.email, subject: 'Nouveau participant')    
+    mail(to: @admin.email, subject: "[#{@event.title}]Nouveau participant")    
   end
 
 end
